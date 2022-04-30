c04c70ad83a11d9c11aa11cf604276621d3823b4
Marius Hambro Bovbjerg <marius@hambro.eu>
2022-04-30 16:38:05 +0200

    yo

--- ExternalService/ExternalService/Program.cs
+++ ExternalService/ExternalService/Program.cs
@@ -13,11 +13,9 @@ builder.Services.AddSingleton<RabbitClient>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
